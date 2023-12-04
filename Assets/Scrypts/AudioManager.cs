9875eff76b74cc2a9797f58c43c7ae0057bf7f89
Taicatsu <nacho_vig@hotmail.com>
2023-12-03 22:47:45 -0300

    Condiciones básicas 1/x

---
+++ Assets/Scrypts/AudioManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header ("Source")]
+
+    public AudioSource MusicSource;
+    public AudioSource SFXSource;
+
+    [Header("Clip")]
+
+    public AudioClip MusicBackground;
+    public AudioClip bottons;
+
+
+    private void Start()
+    {
+        MusicSource.clip = MusicBackground;
+        MusicSource.Play();
+    }
+
+    public void HoverSound (AudioClip clip)
+    {
+        SFXSource.PlayOneShot (bottons);
+    }
+
+}
